ce04036c5d3600404f298e32160eb33dcd1c4078
danubise <danubise@gmail.com>
2018-10-03 11:24:03 +0400

    Добавил логирование

--- ip.php
+++ ip.php
@@ -5,15 +5,18 @@ ini_set('display_errors', 1);
 
 require_once('lib/TCPDF/tcpdf.php');
 require_once('lib/IPTemplate.php');
+require_once ('lib/log.php');
+require_once ('internalconfig.php');
 
+$log = new Log($config);
 $iptemplate = new IPTemplate();
-
+$log->info("Start IP template");
 $data = array();
 
 foreach ($_POST as $key => $post) {
     $key = str_replace("_", ".", $key);
     $data[$key] = $post;
-    echo "key=".$key." => '".$post."'<br>";
+    $log->debug( "key='".$key."' => '".$post."'");
 }
 
 $filename = $iptemplate->showPDF($data);
